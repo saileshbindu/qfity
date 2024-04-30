21c37c9be4b44d9b560d535a2b5a8288647b9d1c
saileshbindu <sailesh.bindu189@gmail.com>
2024-04-30 13:50:20 +0530

    remove nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+// import Navbar from './components/navbar/Navbar';
 import HeroSection from './components/heroSection/Herosection';
 import Cards from './components/cards/Cards';
 
 function App() {
   return (
     <div className="App">
-       <Navbar/>
+       {/* <Navbar/> */}
        <HeroSection />
        <Cards/>
        

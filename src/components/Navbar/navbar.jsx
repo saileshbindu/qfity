6781918ee23331e8c8a8433050e78a1c7068a1ba
saileshbindu <sailesh.bindu189@gmail.com>
2024-04-30 13:30:25 +0530

    remove theme in navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './navbar.css';
-import { useTheme } from '@mui/material/styles';
 import SearchBar from '../searchBar/Search';
 
 import Button from '../button/Button';
@@ -9,7 +8,7 @@ import Logo from "../logo/Logo";
 
 
 const Navbar = () =>{
-    const theme = useTheme();
+    
     return(
        <div>
         <nav className="navbarBg">
